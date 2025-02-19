105bcb128ade059ff2f89d348d19a30f8e5069d7
Omusubi0123 <Omusubi0123@github.com>
2025-02-19 23:49:04 +0900

    wip: SSTのバックグラウンド動作の途中（後回し）

--- src/alg/speech_recognition_call.py
+++ src/alg/speech_recognition_call.py
@@ -26,3 +26,35 @@ def speech_recognition_call() -> Union[str, None]:
     except Exception as e:
         print(f"Error: {e}")
         return None
+
+
+# TODO: backgroundで音声認識を行う関数のバグ修正
+def callback(recognizer, audio):
+    """音声を取得するたびに実行されるコールバック関数"""
+    try:
+        voice_text = recognizer.recognize_google(audio, language="ja-JP")
+        print(f"text: {voice_text}")
+        if voice_text == "stop":
+            print("The program is terminated.")
+            exit(0)  # プログラムを終了する
+    except sr.UnknownValueError:
+        print("Cannot recognize the voice.")
+    except sr.RequestError:
+        print("Failed to connect to Google service.")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def stt_roop():
+    with sr.Microphone() as source:
+        listener.adjust_for_ambient_noise(source)
+        listener.energy_threshold = THRESHOLD
+        print("Listening...")
+        stop_listening = listener.listen_in_background(source, callback)
+
+        try:
+            while True:
+                pass  # メインスレッドを維持（何か他の処理を入れてもOK）
+        except KeyboardInterrupt:
+            stop_listening(wait_for_stop=False)
+            print("Stopped listening.")
