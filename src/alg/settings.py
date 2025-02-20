1975e745a2a48c40d580bd01bde6b27788bf0697
Omusubi0123 <Omusubi0123@github.com>
2025-02-20 10:47:46 +0900

    feat: DBのenvを設定

--- src/alg/settings.py
+++ src/alg/settings.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     openai_api_key: str = Field(..., alias="OPENAI_API_KEY")
+    
+    db_host: str = Field(..., alias="DB_HOST")
+    db_name: str = Field(..., alias="DB_NAME")
+    db_user: str = Field(..., alias="DB_USER")
+    db_pass: str = Field(..., alias="DB_PASS")
+    db_port: int = Field(..., alias="DB_PORT")
 
     model_config = SettingsConfigDict(env_file=".env")
 
